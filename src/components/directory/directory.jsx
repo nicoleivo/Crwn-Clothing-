345f83f56429e095ddb1435c9802d67402d2cb34
nicoleivo <cucchiettinicolas@gmail.com>
2023-03-02 12:21:14 +0100

    137. Introducing Styled-components

--- src/components/directory/directory.jsx
+++ src/components/directory/directory.jsx
@@ -1,15 +1,17 @@
-import CategoryItem from '../category-item/category-item'
+import { Link } from 'react-router-dom';
 
-import './directory.styles.scss'
+import DirectoryItem from '../directory-item/directory-item';
+
+import './directory.styles.scss';
 
 const Directory = ({ categories }) => {
-    return (
-        <div className='directory-container'>
-            {categories.map((category) => (
-                <CategoryItem key={category.id} category={category}/>
-            ))}
-        </div>
-    )
-}
+  return (
+    <div className='directory-container'>
+      {categories.map((category) => (
+        <DirectoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+};
 
-export default Directory
+export default Directory;
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,22 +1,16 @@
-import { useContext } from 'react'
+import { Route, Routes } from 'react-router-dom';
+import CategoriesPreview from '../categories-preview/categories-preview.component';
+import Category from '../category/category.component';
 
-import CategoryPreview from '../../components/category-preview/category-preview.component';
-
-import { CategoriesContext } from '../../contexts/categories.context'
-
-import './shop.styles.scss'
+import './shop.styles.scss';
 
 const Shop = () => {
-    const { categoriesMap } = useContext(CategoriesContext);
+  return (
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=':category' element={<Category />} />
+    </Routes>
+  );
+};
 
-    return (
-        <div className='shop-container'>
-            {Object.keys(categoriesMap).map((title) => {
-                const products = categoriesMap[title]
-                return <CategoryPreview key={title} title={title} products={products}/>
-            })}
-        </div>
-    )
-}
-
-export default Shop
+export default Shop;
